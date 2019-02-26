54f0b0203543fe515468cb29e5cf93983dce5e90
Alexandros Koutroulis <icyd3mon@gmail.com>
2019-02-26 10:40:05 +0200

    Beautification
    New use statements after moving the Property Traits in new directory
    Comments
    PSR-2

--- src/Properties/AbstractProperty.php
+++ src/Properties/AbstractProperty.php
@@ -4,8 +4,13 @@ declare(strict_types=1);
 namespace MisterIcy\ExcelWriter\Properties;
 
 use MisterIcy\ExcelWriter\Exceptions\PropertyException;
+use MisterIcy\ExcelWriter\Properties\Traits\CallableTrait;
+use MisterIcy\ExcelWriter\Properties\Traits\FormatTrait;
+use MisterIcy\ExcelWriter\Properties\Traits\FormulaTrait;
+use MisterIcy\ExcelWriter\Properties\Traits\HeaderTrait;
 use Symfony\Component\PropertyAccess\PropertyAccess;
 use Symfony\Component\PropertyAccess\PropertyAccessor;
+use Error;
 
 /**
  * Class AbstractProperty
@@ -15,8 +20,13 @@ abstract class AbstractProperty implements PropertyInterface
 {
     use FormulaTrait, FormatTrait, HeaderTrait, CallableTrait;
 
-    /** @var PropertyAccessor */
+    /**
+     * A static property accessor to read data from arrays/objects
+     *
+     * @var PropertyAccessor
+     */
     protected static $accessor;
+
     /**
      * Property Path
      *
@@ -34,53 +44,82 @@ abstract class AbstractProperty implements PropertyInterface
 
     /**
      * @param string $path
-     * @return AbstractProperty
+     * @return self
      */
-    public function setPath(string $path): AbstractProperty
+    public function setPath(string $path): self
     {
         $this->path = $path;
         return $this;
     }
 
-    public function isCallable()
+    /**
+     * Checks if we passed a callable in the property, thus
+     * making it callable - a custom function must be executed
+     * before rendering the final value
+     *
+     * @return bool
+     */
+    public function isCallable() : bool
     {
         return (!is_null($this->callable));
     }
+
+    /**
+     * Renders a property's value
+     *
+     * @param object $object
+     * @return mixed
+     * @throws PropertyException
+     */
     public function renderProperty(object $object)
     {
 
         if ($this->isCallable()) {
-            return $this->handleCallable(
-                $this->getValue($object)
-            );
+            return $this->handleCallable($this->getValue($object));
         }
+
         if ($this->isFormula) {
             return strval($this->getFormula());
         }
+
         return $this->getValue($object);
     }
 
-    final protected static function getAccessor() {
+    /**
+     * Gets the Property Accessor, in a singleton way
+     *
+     * @return PropertyAccessor
+     */
+    final protected static function getAccessor()
+    {
         if (null === static::$accessor) {
             static::$accessor = PropertyAccess::createPropertyAccessor();
         }
         return static::$accessor;
     }
 
+    /**
+     * Checks if a property is valid and readable
+     *
+     * @param object $object
+     * @return bool
+     * @throws Error
+     */
     public function checkProperty(object $object): bool
     {
-
         if (static::getAccessor()->isReadable($object, $this->getPath())) {
             return true;
         }
         $objectIdentifier = method_exists($object, '__toString') ?
             call_user_func_array([$object, '__toString'], []) : get_class($object);
-        throw new PropertyException(
+        throw new Error(
             sprintf("I couldn't read property %s of %s", $this->getPath(), $objectIdentifier)
         );
     }
 
     /**
+     * Gets the value of a specific object's property
+     *
      * @param object $object
      * @return mixed
      * @throws PropertyException
