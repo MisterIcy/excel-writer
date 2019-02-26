fad2773fd81d289df5ac15403bdc41613e33c9df
Alexandros Koutroulis <icyd3mon@gmail.com>
2019-02-26 15:26:16 +0200

    Fixed a bug in the reference DataHandler, where it seemed to skip certain formulas that didn't contain path parameters

--- src/Handlers/DataHandler.php
+++ src/Handlers/DataHandler.php
@@ -31,7 +31,9 @@ final class DataHandler extends AbstractHandler
         foreach ($data as $object) {
             /** @var AbstractProperty $property */
             foreach ($properties->getProperties() as $property) {
+
                 $value = $property->renderProperty($object);
+
                 if ($property->isFormula()) {
                     $value = str_replace("{row}", strval($row), $value);
                     $value = str_replace("{col}", Coordinate::stringFromColumnIndex($column), $value);
@@ -39,39 +41,31 @@ final class DataHandler extends AbstractHandler
                     $matches = [];
                     preg_match_all("/\[(.*?)\]/", $value, $matches, PREG_UNMATCHED_AS_NULL);
 
+
                     if (count($matches) > 0) {
-                        if (count($matches[0]) === 0 && count($matches[1]) === 0) {
-                            continue;
+                        if (count($matches[0]) > 0 && count($matches[1]) > 0) {
+                            foreach ($matches[1] as $match) {
+                                $propColumn = $properties->getExcelColumnOfPropertyPath($match);
+                                $value = str_replace("[" . $match . "]", $propColumn, $value);
+                            }
                         }
-                    }
-                    foreach ($matches[1] as $match) {
-                        $propColumn = $properties->getExcelColumnOfPropertyPath($match);
-                        $value = str_replace("[" . $match . "]", $propColumn, $value);
                     }
                 }
                 $spreadsheet->getActiveSheet()
                     ->setCellValueByColumnAndRow($column, $row, $value);
 
-                $this->formatCell(
-                    $spreadsheet->getActiveSheet()->getCellByColumnAndRow($column, $row),
-                    $property
-                );
+
+                $spreadsheet
+                    ->getActiveSheet()
+                    ->getCellByColumnAndRow($column, $row)
+                    ->getStyle()
+                    ->getNumberFormat()
+                    ->setFormatCode($property->getFormatCode());
                 $column++;
             }
             $column = 1;
             $row++;
         }
         return parent::handle($generator);
-    }
-
-    /**
-     * @param Cell $cell
-     * @param AbstractProperty $property
-     */
-    private function formatCell(Cell $cell, AbstractProperty $property)
-    {
-        $cell->getStyle()
-            ->getNumberFormat()
-            ->setFormatCode($property->getFormatCode());
     }
 }
