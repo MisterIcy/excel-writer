1063f083b8b94f360e861c4ad4b49a7dc0714fd8
Alexandros Koutroulis <icyd3mon@gmail.com>
2019-02-26 10:17:15 +0200

    added $nullAllowed to DateTimeTrait

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+# Changelog
+
+## 0.1.1
+### Added:
+ - DateTimeTrait::$nullAllowed : Allows the developer to specify null values or other objects and get null values
+   
+
+## 0.1
+ - Initial Version
---
+++ src/Properties/Traits/DateTimeTrait.php
@@ -0,0 +1,101 @@
+<?php
+declare(strict_types=1);
+
+namespace MisterIcy\ExcelWriter\Properties;
+
+use PhpOffice\PhpSpreadsheet\Shared\Date;
+use DateTimeInterface;
+use TypeError;
+/**
+ * Trait DateTimeTrait
+ * @package MisterIcy\ExcelWriter\Properties
+ *
+ * @author Alexandros Koutroulis
+ * @license MIT
+ */
+trait DateTimeTrait
+{
+    /**
+     * Allows null dates or other artifacts that are not of type {@see DateTimeInterface}.
+     * @var bool
+     */
+    private $nullAllowed = false;
+
+    /**
+     * @return bool
+     */
+    public function isNullAllowed(): bool
+    {
+        return $this->nullAllowed;
+    }
+
+    /**
+     * @param bool $nullAllowed
+     * @return self
+     */
+    public function setNullAllowed(bool $nullAllowed)
+    {
+        $this->nullAllowed = $nullAllowed;
+        return $this;
+    }
+
+    /**
+     * Coverts a DateTime object to Excel Time (float)
+     *
+     * @param DateTimeInterface $object
+     * @return float
+     * @throws TypeError
+     */
+    public function convertDateTimeToExcelTime(DateTimeInterface $object) : float
+    {
+        //This is unrecoverable - we can't just assume Null.
+        //The developer must handle this error.
+        if (!$object instanceof DateTimeInterface) {
+            if ($this->isNullAllowed()) {
+                return 0;
+            }
+            throw new TypeError("Invalid date object supplied to datetime converter");
+        }
+
+        $hour = intval($object->format('H'));
+        $minute = intval($object->format('i'));
+        $second = intval($object->format('s'));
+
+        return (Date::formattedPHPToExcel(1900, 1,1, $hour, $minute, $second) - 1);
+    }
+
+    /**
+     * Coverts a DateTime object to excel Date (float)
+     *
+     * @param DateTimeInterface $object
+     * @return float
+     * @throws TypeError
+     */
+    public function convertDateTimeToExcelDate(DateTimeInterface $object) : float
+    {
+        //This is unrecoverable - we can't just assume Null.
+        //The developer must handle this error.
+        if (!$object instanceof DateTimeInterface) {
+            if ($this->isNullAllowed()) {
+                return 0;
+            }
+            throw new TypeError("Invalid date object supplied to datetime converter");
+        }
+
+        $year = intval($object->format('Y'));
+        $month = intval($object->format('m'));
+        $day = intval($object->format('d'));
+
+        return Date::formattedPHPToExcel($year, $month, $day);
+    }
+
+    /**
+     * @param DateTimeInterface $object
+     * @return float
+     * @throws TypeError
+     */
+    public function convertDateTimeToExcelDateTime(DateTimeInterface $object) : float
+    {
+        return $this->convertDateTimeToExcelDate($object) + $this->convertDateTimeToExcelTime($object);
+    }
+}
