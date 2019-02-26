b28ba3c518b4dce329ff28eade4f40e7b649ab99
Alexandros Koutroulis <icyd3mon@gmail.com>
2019-02-26 11:34:57 +0200

    Added functions to get an Excel Column (int/string) from a Property

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,7 +3,8 @@
 ## 0.1.1
 ### Added:
  - DateTimeTrait::$nullAllowed : Allows the developer to specify null values or other objects and get null values
- - By default, $nullAllowed is true, to reduce checks.  
+ - By default, $nullAllowed is true, to reduce checks.
+ - Added functions to get an Excel Column (int/string) from a Property  
 
 ## 0.1
  - Initial Version
--- src/Properties/PropertyCollection.php
+++ src/Properties/PropertyCollection.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 namespace MisterIcy\ExcelWriter\Properties;
 
 use Doctrine\Common\Collections\ArrayCollection;
+use http\Encoding\Stream\Inflate;
+use MisterIcy\ExcelWriter\Exceptions\PropertyException;
+use PhpOffice\PhpSpreadsheet\Cell\Coordinate;
 
 /**
  * Class PropertyCollection
@@ -50,5 +53,59 @@ final class PropertyCollection
     public function getProperties()
     {
         return $this->collection;
+    }
+
+    /**
+     * @param PropertyInterface $property
+     * @return int
+     * @throws PropertyException
+     */
+    public function getColumnOfProperty(PropertyInterface $property) : int
+    {
+        if ($this->getProperties()->contains($property)) {
+            return $this->getProperties()->indexOf($property) +1;
+        }
+        throw new PropertyException("The specified property is not a part of the collection");
+    }
+
+    /**
+     * @param PropertyInterface $property
+     * @return string
+     * @throws PropertyException
+     */
+    public function getExcelColumnOfProperty(PropertyInterface $property) : string
+    {
+        return Coordinate::stringFromColumnIndex($this->getColumnOfProperty($property));
+    }
+
+    /**
+     * @param string $propertyPath
+     * @return int
+     * @throws PropertyException
+     */
+    public function getColumnOfPropertyPath(string $propertyPath) : int
+    {
+        /**
+         * @var int $key
+         * @var AbstractProperty $property
+         */
+        foreach ($this->getProperties()->toArray() as $key => $property) {
+            if (strcmp($propertyPath, $property->getPath())) {
+                return $this->getColumnOfProperty($property);
+            }
+        }
+        throw new PropertyException("The specified property path is not found in any property of the collection");
+    }
+
+    /**
+     * @param string $propertyPath
+     * @return string
+     * @throws PropertyException
+     */
+    public function getExcelColumnOfPropertyPath(string $propertyPath) : string
+    {
+        return $this->getExcelColumnOfProperty(
+            $this->getColumnOfPropertyPath($propertyPath)
+        );
     }
 }
